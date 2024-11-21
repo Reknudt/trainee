fe74806216aa9a9ee1a6c68ecb011988867f1842
Reknudt <strangerforfallen@gmail.com>
2024-11-21 15:19:45 +0300

    Test oauth2 keycloak

--- src/main/java/org/pavlov/Apitest.java
+++ src/main/java/org/pavlov/Apitest.java
@@ -2,11 +2,69 @@ package org.pavlov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Apitest {
 
     public static void main(String[] args) {
         SpringApplication.run(Apitest.class, args);
+    }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(
+//                authorizeRequests -> authorizeRequests.requestMatchers("/swagger-ui/**")
+//                        .permitAll()
+//                        .requestMatchers("/v3/api-docs*/**")
+//                        .permitAll());
+//
+//        return http.build();
+//    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http
+                .authorizeHttpRequests(
+                        authorizeRequests -> authorizeRequests.requestMatchers("/swagger-ui/**")
+                                .permitAll()
+                                .requestMatchers("/v3/api-docs*/**")
+                                .permitAll())
+                .authorizeHttpRequests(c -> c.requestMatchers("/error").permitAll()
+                        .requestMatchers("/admin.html").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var converter = new JwtAuthenticationConverter();
+        var jwtGrantedAuthoritiesConverter = new JwtAuthenticationConverter();
+
+        converter.setPrincipalClaimName("preferred_username");
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+
+            var authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+            var roles = jwt.getClaimAsStringList("spring_sec_roles");
+
+            return Stream.concat(authorities.getAuthorities().stream(),
+                            roles.stream()
+                                    .filter(role -> role.startsWith("ROLE_"))
+                                    .map(SimpleGrantedAuthority::new)
+                                    .map(GrantedAuthority.class::cast))
+                    .toList();
+
+        });
+        return converter;
     }
 }
